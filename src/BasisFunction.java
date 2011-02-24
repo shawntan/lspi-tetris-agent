99ee67a6de7f8352df4dba22c392f21e3bdecd38
shawntan <shawn@wtf.sg>
2011-02-24 17:48:04 +0800

    Implemented LSPI but it doesn't work!

--- src/BasisFunction.java
+++ src/BasisFunction.java
@@ -1,10 +1,10 @@
+import java.util.Arrays;
 
 public class BasisFunction {
-	
-	private static int count = 0;
-	
+	final private static double DISCOUNT = 1;
+
+	private static int count = 0;	
 	//feature list
-	final private static int CONSTANT				= count++;
 	final private static int ROWS_COMPLETED 		= count++;
 	final private static int DIFF_ROWS_COMPLETED	= count++;
 	final private static int MAX_HEIGHT				= count++;
@@ -16,12 +16,11 @@ public class BasisFunction {
 	final private static int COVERED_GAPS			= count++;
 	final private static int DIFF_COVERED_GAPS		= count++;
 	final private static int NEXT_MOVE_LOSE			= count++;
-	
+
 	final public static int FEATURE_COUNT = count;
 	static double[] weight = new double[FEATURE_COUNT];
 	static {
 		//initial weights
-		weight[CONSTANT]			=	1;
 		weight[ROWS_COMPLETED]		=	2;
 		weight[DIFF_ROWS_COMPLETED] =	2;
 		weight[MAX_HEIGHT]			=	0;
@@ -34,26 +33,27 @@ public class BasisFunction {
 		weight[DIFF_COVERED_GAPS]	=	-4;
 		weight[NEXT_MOVE_LOSE]		=	-10;
 	}
-	
+
 
 	private static double[] features = new double[FEATURE_COUNT]; 
 	private static double[] future = new double[FEATURE_COUNT];
-	
+
+
 	public static double[] getFeatureArray(State s, FutureState fs) {
 		//simple features
-		features[CONSTANT] = 1;
 		features[ROWS_COMPLETED] = s.getRowsCleared();
 		features[DIFF_ROWS_COMPLETED] = fs.getRowsCleared()-features[ROWS_COMPLETED];
-		
+
 		//compute height features
 		heightFeatures(s, features);
 		heightFeatures(fs, future);
-		
+
 		features[DIFF_MAX_HEIGHT] = future[MAX_HEIGHT]-features[MAX_HEIGHT];
 		features[DIFF_AVG_HEIGHT] = future[AVG_HEIGHT]-features[AVG_HEIGHT];
 		features[DIFF_SUM_ADJ_DIFF] = future[SUM_ADJ_DIFF]-features[SUM_ADJ_DIFF];
 		features[DIFF_COVERED_GAPS] = future[COVERED_GAPS]-features[COVERED_GAPS];
 		features[NEXT_MOVE_LOSE] = fs.hasLost()?1:0;
+
 		return features;
 	}
 
@@ -67,7 +67,7 @@ public class BasisFunction {
 		int prev=0;//holds the previous column's height
 		//covered gaps
 		int coveredGaps=0;
-		
+
 		int[][] field = s.getField();
 		for(int j=0;j<field[0].length;j++){ //by column
 			int i=field.length-1;
@@ -88,4 +88,56 @@ public class BasisFunction {
 		vals[SUM_ADJ_DIFF] = diffTotal;
 		vals[COVERED_GAPS] = coveredGaps;
 	}
+
+
+	private static double[][] featureProduct = new double[FEATURE_COUNT][FEATURE_COUNT];
+
+	private static double[][] A = new double[FEATURE_COUNT][FEATURE_COUNT];
+	private static double[][] tmp = new double[FEATURE_COUNT][FEATURE_COUNT];
+	private static double[][] aInverse = new double[FEATURE_COUNT][FEATURE_COUNT];
+
+	private static double[] b = new double[FEATURE_COUNT];
+
+	private static double[] tmpF = new double[FEATURE_COUNT];
+	private static double[] tmpPF = new double[FEATURE_COUNT];
+	public static void updateWeights(double[] prevFeatures,double[] features) {
+		System.arraycopy(features, 0,tmpF,0,FEATURE_COUNT);
+		System.arraycopy(prevFeatures, 0,tmpPF,0,FEATURE_COUNT);
+		System.out.println("f:\t\t"+Arrays.toString(features));
+		System.out.println("pf:\t\t"+Arrays.toString(prevFeatures));
+		System.out.println("--------------------------------------");
+
+		System.out.println("pf:\t\t"+Arrays.toString(tmpPF));
+		Matrix.multiply(-1*DISCOUNT,tmpF);
+		System.out.println("df:\t\t"+Arrays.toString(tmpF));
+		Matrix.sum(tmpPF,tmpF);
+		System.out.println("delta phi:\t"+Arrays.toString(tmpPF));
+		Matrix.product(prevFeatures,tmpPF,featureProduct);
+		System.out.println("delta A:");
+		printField(featureProduct);
+		Matrix.sum(A, featureProduct);
+		System.arraycopy(prevFeatures, 0,tmpPF,0,FEATURE_COUNT);
+		Matrix.sum(b, Matrix.multiply(prevFeatures[DIFF_ROWS_COMPLETED], tmpPF));
+		System.out.println("b:\t"+Arrays.toString(b));
+		
+		if(Matrix.inverse(A,aInverse,tmp)==null) return;
+		else {
+			System.out.println("A-1:");
+			printField(aInverse);
+			Matrix.product(aInverse, b, weight); 
+
+			System.out.println(Arrays.toString(weight));
+		}
+	}
+
+
+	private static void printField(double[][] field){
+		for(int i=0;i<field.length;i++) {
+			for(int j=0;j<field[0].length;j++){
+				System.out.print(" "+field[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
 }
--- src/PlayerSkeleton.java
+++ src/PlayerSkeleton.java
@@ -1,15 +1,14 @@
 import java.text.DecimalFormat;
-import java.util.Arrays;
 
 
 
 
 public class PlayerSkeleton {
 	//implement this function to have a working system
-	
+
 	private FutureState fs;
 	private DecimalFormat formatter = new DecimalFormat("#00");
-
+	private boolean firstMove = true;
 	public int pickMove(State s, int[][] legalMoves) {
 		if(fs==null) fs = new FutureState();
 		fs.resetToCurrentState(s);
@@ -22,27 +21,44 @@ public class PlayerSkeleton {
 			System.out.println("---------------");
 			printField(fs.getField());
 			System.out.println("---------------");
-			*/
-			score = score(BasisFunction.getFeatureArray(s, fs));
-			
+			 */
+			double[] f = BasisFunction.getFeatureArray(s, fs);
+			score = score(f);
 			if(maxScore < score) {
 				maxScore = score;
 				maxMove = m;
-
+				System.arraycopy(f,0,features,0,f.length);
 			}
 			//System.out.println(maxScore);
 			fs.resetToCurrentState(s);
 		}
+		if(firstMove){
+			firstMove = false;
+			swapFArrays();
+		}
+		else updateWeights();
 		return maxMove;
 	}
-	
+	private double[] prevFeatures = new double[BasisFunction.FEATURE_COUNT];
+	private double[] features = new double[BasisFunction.FEATURE_COUNT];
+	private void swapFArrays() {
+		double[] tmp = prevFeatures;
+		prevFeatures = features;
+		features = tmp;
+	}
+	private void updateWeights(){
+		BasisFunction.updateWeights(prevFeatures, features);
+		swapFArrays();
+	}
 	private double score(double[] features){
 		//System.out.println(Arrays.toString(features));
 		double total=0;
 		for(int i=0;i<features.length;i++) total+=features[i]*BasisFunction.weight[i];
 		return total;
 	}
-	
+
+
+
 	private void printField(int[][] field){
 		for(int i=0;i<field.length;i++) {
 			for(int j=0;j<field[0].length;j++){
@@ -51,22 +67,27 @@ public class PlayerSkeleton {
 			System.out.println();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		State s = new State();
-		new TFrame(s);
-		PlayerSkeleton p = new PlayerSkeleton();
-		while(!s.hasLost()) {
-			s.makeMove(p.pickMove(s,s.legalMoves()));
-			s.draw();
-			s.drawNext(0,0);
-			try {
-				Thread.sleep(300);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		
+		for(int i=0;i<1;i++){
+			State s = new State();
+			TFrame t = new TFrame(s);
+			PlayerSkeleton p = new PlayerSkeleton();
+			while(!s.hasLost()) {
+				s.makeMove(p.pickMove(s,s.legalMoves()));
+				s.draw();
+				s.drawNext(0,0);
+				try {
+					Thread.sleep(300);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
+			System.out.println("You have completed "+s.getRowsCleared()+" rows.");
+			//t.dispose();
+			//t.setVisible(false);
 		}
-		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
 	}
-	
+
 }
