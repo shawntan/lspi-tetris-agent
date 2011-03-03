78bf5de94e5acd389e100b0fbc5e713d4fb74023
shawntan <shawn@wtf.sg>
2011-03-03 23:45:02 +0800

    After f

--- src/BasisFunction.java
+++ src/BasisFunction.java
@@ -1,31 +1,35 @@
 public class BasisFunction {
-	final private static double DISCOUNT = 0.9;
+	final private static double DISCOUNT = 0.99;
 
 	private static int count = 0;	
 	//feature list
 	final private static int ROWS_COMPLETED 		= count++;
 	final private static int DIFF_ROWS_COMPLETED	= count++;
 	final private static int MAX_HEIGHT				= count++;
-	//final private static int MIN_HEIGHT				= count++;
 	final private static int DIFF_MAX_HEIGHT		= count++;
+	final private static int MIN_HEIGHT				= count++;
+	final private static int DIFF_MIN_HEIGHT		= count++;
 	final private static int AVG_HEIGHT				= count++;
 	final private static int DIFF_AVG_HEIGHT		= count++;
 	final private static int SUM_ADJ_DIFF			= count++;
 	final private static int DIFF_SUM_ADJ_DIFF		= count++;
 	final private static int COVERED_GAPS			= count++;
 	final private static int DIFF_COVERED_GAPS		= count++;
+	final private static int TOTAL_BLOCKS			= count++;
+	final private static int TOTAL_WELL_DEPTH		= count++;
+	final private static int DIFF_TOTAL_WELL_DEPTH	= count++;
+	final private static int MAX_WELL_DEPTH			= count++;
 	
-	//final private static int NEXT_MOVE_LOSE			= count++;
-
 	final public static int FEATURE_COUNT = count;
+	
 	static double[] weight = new double[FEATURE_COUNT];
 	static {
 
 		//initial weights
+/*
 		weight[ROWS_COMPLETED]		=	2;
 		weight[DIFF_ROWS_COMPLETED] =	2;
 		weight[MAX_HEIGHT]			=	0;
-		//weight[MIN_HEIGHT]			=	0;
 		weight[DIFF_MAX_HEIGHT]		=	-2;
 		weight[AVG_HEIGHT]			=	0;
 		weight[DIFF_AVG_HEIGHT]		=	-1;
@@ -33,12 +37,28 @@ public class BasisFunction {
 		weight[DIFF_SUM_ADJ_DIFF]	=	-2;
 		weight[COVERED_GAPS]		=	0;
 		weight[DIFF_COVERED_GAPS]	=	-4;
-
+*/
 		//weight[NEXT_MOVE_LOSE]		=	-10;
 
 		weight = new double[] {
-				2.43837989469314E-5, 8.961595503848667, 0.015925823093283387, -0.01682357361806941, 0.4248430008970081, 8.00288696739108, -0.08465044114870991, -0.003900189826416073, -0.3660016454753236, -0.8824617779319843		};
-
+				2.569079102888311E-5,
+				91.92573819667726,
+				-0.04681056168461907,
+				-0.0021226395604664653,
+				0.006003403006131504,
+				1.361816288777823E-4,
+				1.8818215988751974,
+				90.76001747787525,
+				-0.018794800985089267,
+				2.7172078654351048E-5,
+				-0.7279972238516823,
+				-9.086153462279242,
+				-0.09184812023609065,
+				0.017283723692958077, 
+				-0.07938074313737384,
+				0
+		};
+	
 	}
 
 
@@ -56,11 +76,11 @@ public class BasisFunction {
 		heightFeatures(fs, features);
 
 		features[DIFF_MAX_HEIGHT] = 	features[MAX_HEIGHT]-past[MAX_HEIGHT];
+		features[DIFF_MIN_HEIGHT] = 	features[MIN_HEIGHT]-past[MIN_HEIGHT];
 		features[DIFF_AVG_HEIGHT] = 	features[AVG_HEIGHT]-past[AVG_HEIGHT];
 		features[DIFF_SUM_ADJ_DIFF] = 	features[SUM_ADJ_DIFF]-past[SUM_ADJ_DIFF];
 		features[DIFF_COVERED_GAPS] =	features[COVERED_GAPS]-past[COVERED_GAPS];
-		//features[NEXT_MOVE_LOSE] = fs.hasLost()?1:0;
-
+		features[DIFF_TOTAL_WELL_DEPTH] =	features[TOTAL_WELL_DEPTH]-past[TOTAL_WELL_DEPTH];
 		return features;
 	}
 
@@ -76,28 +96,40 @@ public class BasisFunction {
 		int prev=0;//holds the previous column's height
 		//covered gaps
 		int coveredGaps=0;
-
+		int totalBlocks = 0;
+		int totalWellDepth = 0;
+		int maxWellDepth = 0;
 		int[][] field = s.getField();
 		for(int j=0;j<field[0].length;j++){ //by column
 			int i=field.length-1;
-			while(i>=0 && field[i][j]==0) i--; //go down the column till first element found
+			int wellDepth = 0;
+			while(i>=0 && field[i][j]==0) {
+				if(		(j==0				||field[i][j-1]!=0) && 
+						(j==field[0].length-1||field[i][j+1]!=0)) wellDepth++; 
+				i--;
+			} //go down the column till first element found
 			int height = i+1;
 			if(maxHeight<height) maxHeight = height;
-			if(minHeight>height) minHeight = height;
+			if(maxWellDepth<wellDepth) maxWellDepth = wellDepth;
+			else if(minHeight>height) minHeight = height;
 			total += height;
-			if(j>0) diffTotal += Math.abs((height)-prev);
+			totalWellDepth += wellDepth;
+			if(j>0) diffTotal += Math.pow(height-prev,2);
 			prev = height;
-			i--; //skip the first element.
-			while(i>=0) {
-				if(field[i][j]==0) coveredGaps++;
+			while(i>0) {
 				i--;
+				if(field[i][j]==0) coveredGaps++;
+				else totalBlocks++;
 			}
 		}
-		
+		vals[MAX_WELL_DEPTH] = maxWellDepth;
 		vals[MAX_HEIGHT] = maxHeight;
-		vals[AVG_HEIGHT] = total/(double)field[0].length;
+		vals[MIN_HEIGHT] = minHeight;
+		vals[AVG_HEIGHT] = ((double)total/field[0].length);
 		vals[SUM_ADJ_DIFF] = diffTotal;
 		vals[COVERED_GAPS] = coveredGaps;
+		vals[TOTAL_BLOCKS] = totalBlocks;
+		vals[TOTAL_WELL_DEPTH] = totalWellDepth;
 	}
 
 	public static double[][] A = new double[FEATURE_COUNT][FEATURE_COUNT];
@@ -123,6 +155,7 @@ public class BasisFunction {
 		Matrix.product(mFeatures,mRowFeatures,changeToA);
 		Matrix.sum(A,changeToA);
 		Matrix.multiply(features[DIFF_ROWS_COMPLETED], mFeatures);
+		
 		Matrix.sum(b,mFeatures);
 		/*
 		System.out.println("A:");
--- src/PlayerSkeleton.java
+++ src/PlayerSkeleton.java
@@ -9,42 +9,53 @@ public class PlayerSkeleton {
 
 	private FutureState fs;
 	private DecimalFormat formatter = new DecimalFormat("#00");
-	
+
 	private double[] feature = new double[BasisFunction.FEATURE_COUNT];
 	private double[] past = null;
-	
+
 	public int pickMove(State s, int[][] legalMoves) {
 		if(fs==null) fs = new FutureState();
 		fs.resetToCurrentState(s);
 		int maxMove = pickBestMove(s, legalMoves, feature);
 		//simulate next step
 		if(past == null) past = new double[BasisFunction.FEATURE_COUNT]; 
-		else BasisFunction.updateMatrices(s, past, feature);
+		else {
+			BasisFunction.updateMatrices(s, past, feature);
+			//BasisFunction.computeWeights();
+			//if(count>100) BasisFunction.computeWeights();
+		}
 		double[] tmp = feature;
 		feature = past;
 		past = tmp;
 		return maxMove;
 	}
-	
+
 	public int pickBestMove(State s, int[][] legalMoves, double[] feature){
 		double score;
 		double maxScore = Double.NEGATIVE_INFINITY;
-		int maxMove = -1;
-		for(int m = 0;m<legalMoves.length;m++){
+
+		int d = legalMoves.length;
+		int init = (int)(Math.random()*d);
+		int m = (init+1)%d;
+		int maxMove = m ;
+		while(m != init){
 			fs.makeMove(m);
-			double[] f = BasisFunction.getFeatureArray(s, fs);
-			score = score(f);
-			if(maxScore < score) {
-				maxScore = score;
-				maxMove = m;
-				System.arraycopy(f,0,feature,0,f.length);
+			if(!fs.hasLost()) {
+				double[] f = BasisFunction.getFeatureArray(s, fs);
+				score = score(f);
+				if(maxScore < score) {
+					maxScore = score;
+					maxMove = m;
+					System.arraycopy(f,0,feature,0,f.length);
+				}
 			}
 			fs.resetToCurrentState(s);
+			m = (m+1)%d;
 		}
 		return maxMove;
 	}
 
-	
+
 	private double score(double[] features){
 		//System.out.println(Arrays.toString(features));
 		double total=0;
@@ -74,20 +85,20 @@ public class PlayerSkeleton {
 		System.out.println(Arrays.toString(BasisFunction.weight));
 		State s = new State();
 		TFrame t = new TFrame(s);
-		
+
 		while(!s.hasLost()) {
 			s.makeMove(p.pickMove(s,s.legalMoves()));
 			s.draw();
 			s.drawNext(0,0);
 			try {
-				Thread.sleep(300);
+				Thread.sleep(30);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			//BasisFunction.computeWeights();
 		}
 		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
-		
+
 	}
 
 }
